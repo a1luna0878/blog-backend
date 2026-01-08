f835707edd07123b3e63a52cc39191a2b1ce0ecd
iskoemes <ryskeldimyrzaliev20@icloud.com>
2026-01-08 16:15:43 +0600

    Fix TypeORM config for production

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,9 @@
-
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthModule } from './auth/auth.module';
-import { CategoriesController } from './categories/categories.controller';
-import { CategoriesService } from './categories/categories.service';
-import { ArticlesController } from './articles/articles.controller';
-import { ArticlesService } from './articles/articles.service';
-import { User } from './auth/user.entity';
-import { Category } from './categories/category.entity';
-import { Article } from './articles/article.entity';
 import { CategoriesModule } from './categories/categories.module';
 import { ArticlesModule } from './articles/articles.module';
 
@@ -20,18 +13,13 @@ import { ArticlesModule } from './articles/articles.module';
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      port: 5432,
-      username: 'postgres', // Дефолтный юзер
-      password: 'root', // Твой пароль из установки (или '' если без)
-      database: 'blog', // БД, которую создал
-      entities: [User, Category, Article],
-      synchronize: true, // Авто-создаёт таблицы (только для dev!)
+      synchronize: true, // OK для тестового
     }),
     AuthModule,
     CategoriesModule,
     ArticlesModule,
   ],
-  controllers: [AppController, CategoriesController, ArticlesController],
-  providers: [AppService, CategoriesService, ArticlesService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
