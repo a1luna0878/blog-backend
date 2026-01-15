56a17ec073c8c09d2a2be9ab4de6b0341e40588b
iskoemes <ryskeldimyrzaliev20@icloud.com>
2026-01-15 14:20:18 +0600

    fix: build5

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -14,33 +14,38 @@ import { JwtAuthGuard } from '../auth/auth.guard';
 
 @Controller('articles')
 export class ArticlesController {
-  constructor(private service: ArticlesService) {}
+  constructor(private readonly articlesService: ArticlesService) {}
 
+  // CREATE ARTICLE (protected)
   @Post()
   @UseGuards(JwtAuthGuard)
   create(@Body() dto: CreateArticleDto) {
-    return this.service.create(dto);
+    return this.articlesService.create(dto);
   }
 
+  // GET ALL ARTICLES (public)
   @Get()
   findAll() {
-    return this.service.findAll();
+    return this.articlesService.findAll();
   }
 
+  // GET ONE ARTICLE BY ID (public)
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.service.findOne(id);
+    return this.articlesService.findOne(id);
   }
 
+  // UPDATE ARTICLE (protected)
   @Put(':id')
   @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() dto: CreateArticleDto) {
-    return this.service.update(id, dto);
+    return this.articlesService.update(id, dto);
   }
 
+  // DELETE ARTICLE (protected)
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
   delete(@Param('id') id: string) {
-    return this.service.delete(id);
+    return this.articlesService.delete(id);
   }
 }
